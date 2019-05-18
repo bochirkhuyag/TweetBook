f12942d6a772b8392ce0725906230c5ba10041e3
DmaC <demon.005.gl@gmail.com>
2019-05-18 17:14:08 -0500

    project structure generating components generated empty AppGuard service style imports routing

--- app-client/src/app/app-routing.module.ts
+++ app-client/src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResgisterComponent } from './user/resgister/resgister.component';
 import { LoginComponent } from './user/login/login.component';
+import {AppGuardService} from "./services/app-guard.service";
+import {HomeComponent} from "./home/home.component";
+import {WallComponent} from "./wall/wall.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
-  {path:"", component:ResgisterComponent},
-  {path:"login", component:LoginComponent}
+  {path:"login", component:LoginComponent},
+  {path:"register", component:ResgisterComponent},
+  //wall
+  {path: '', component: HomeComponent, children: [
+      {path: '', component: WallComponent, canActivate: [AppGuardService]},
+      {path: 'profile', component: ProfileComponent, canActivate: [AppGuardService]}
+  ]}
 ];
 
 @NgModule({
--- app-client/src/app/app.module.ts
+++ app-client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { WallComponent } from './wall/wall.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    WallComponent,
+    ProfileComponent
 
   ],
   imports: [
