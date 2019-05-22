0a4f6f9039184f19f5afc7b91918145e1ccb86fc
Thanh Tran <hoaithanhht3@gmail.com>
2019-05-22 09:50:02 -0500

    turn verify off

--- app-server/middleware/verifyToken.js
+++ app-server/middleware/verifyToken.js
@@ -1,11 +1,11 @@
-
+const turnoff = true;
 const verifyToken = function(){
     return function(req, res, next){
         console.log("verify token");
                 const authorizationHeader = req.headers['authorization'];
-                if( authorizationHeader !== undefined ){
+                if( authorizationHeader !== undefined || turnoff){
                     //get token                    
-                    req.token = authorizationHeader.split(' ')[1];
+                    //req.token = authorizationHeader.split(' ')[1];
                     next();
                 } else {
                     res.sendStatus(403);
